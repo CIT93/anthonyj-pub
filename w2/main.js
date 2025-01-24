11a9ef7b87664107a86098599a8539791b14671d
ajsafedrive <156267981+ajsafedrive@users.noreply.github.com>
2025-01-24 07:19:12 +0000

    coding is done

--- w2/main.js
+++ w2/main.js
@@ -1,1 +1,24 @@
-console.log("Week 2 - feeling the pressure");
+// How to calculate 
+
+// 1. # Household Members
+const houseMembers = 6;
+// 2. Home size
+const homeSize = 4;
+// 3. Food choices
+const foodChoices = 8;
+// 4. Water Intake
+const waterIntake = 2;
+// 5. Household Purchases
+const householdPurchases = 10;
+// 6. Waste Production
+const wasteProduction = 20;
+// 7. Recycling Effort
+const recyclingEffort = 8;
+// 8. Annual Transport
+const annualTransport = 6;
+// 9. Totals
+const totalHouseholdCalculation = houseMembers + homeSize + foodChoices + waterIntake + householdPurchases + wasteProduction + recyclingEffort + annualTransport; 
+
+// H1
+const cfpTotal = document.querySelector("h1"); 
+cfpTotal.textContent = totalHouseholdCalculation;
