2fd971f2d3b93ca3a4b3bc64cbdc31c255fde883
ajsafedrive <156267981+ajsafedrive@users.noreply.github.com>
2025-03-04 00:38:39 +0000

    my code

--- w5/main.js
+++ w5/main.js
@@ -1,7 +1,44 @@
 
-
+const TBL = document.getElementById("movieList");
 const movieData = [];
 
+
+function renderTblHeading () {
+    const table = document.createElement("table");
+     const thead = document.createElement("thead");
+     const tr = document.createElement("tr");
+     const headingTextArr = ["Movie", "Year", "Rating"];
+     headingTextArr.forEach(function (text) {
+       const th = document.createElement("th");
+       th.textContent = text;
+       tr.appendChild(th);
+     });
+     thead.appendChild(tr);
+     table.appendChild(thead);
+     return table
+   }
+
+   function renderTbl(data) {
+
+    const table = renderTblHeading();
+    const tbody = document.createElement("tbody");
+    data.forEach(function (obj) {      
+      const tr = document.createElement("tr");
+      const tdMovie = document.createElement("td");
+      tdMovie.textContent = obj.movie
+      const tdYear = document.createElement("td");
+      tdYear.textContent = obj.year
+      tr.appendChild(tdMovie);
+      tr.appendChild(tdYear) 
+      const tdRating = document.createElement("td")
+      tdRating.textContent = obj.rating
+      tr.appendChild(tdRating)
+      tbody.appendChild(tr);
+    });
+   
+    table.appendChild(tbody);
+    TBL.appendChild(table);
+  }
 
 function start() {
      movieData.push(
@@ -11,8 +48,9 @@ function start() {
      )
 }
 
-function displayMovieList(){
+/* function displayMovieList(){
 const movieList = document.getElementById("movieList");
+movieList.appendChild(document.createElement("th")).textContent = "Movie";
 movieData.forEach((movie) => {
     console.log(movie.movie)
     console.log(movie.year)
@@ -35,106 +73,10 @@ movieData.forEach((movie) => {
     
 }
 
+
+
 start()
 displayMovieList();
 
-// OLLLLLLLLLLLLLLLLLLLD CODE
-
-/*
-const cfpData = [];
-function determineHouseSizePts(size) {
-    let houseSizePoints = 0;
-    if(size === "large") {
-      houseSizePoints = 10;
-    } else if(size === "medium") {
-      houseSizePoints = 7;
-    } else if (size === "small") {
-      houseSizePoints = 4;
-    } else if (size === "apt") {
-      houseSizePoints = 2;
-    }
-      return houseSizePoints;
-  }
-  
-  function determineHouseHoldPts(numberInHousehold) {
-    let houseHoldPoints = 0;
-    if (numberInHousehold === 1) {
-      houseHoldPoints = 14;
-    } else if (numberInHousehold === 2) {
-      houseHoldPoints = 12;
-    } else if (numberInHousehold === 3) {
-      houseHoldPoints = 10;
-    } else if (numberInHousehold === 4) {
-      houseHoldPoints = 8;
-    } else if (numberInHousehold === 5) {
-      houseHoldPoints = 6;
-    } else if (numberInHousehold === 6) {
-      houseHoldPoints = 4;
-    } else if (numberInHousehold > 6) {
-      houseHoldPoints = 2;
-    }
-     return houseHoldPoints;
-  }
-  
-  function start(houseHoldMembers, houseSize) {
-    const houseHoldPTS = determineHouseHoldPts(houseHoldMembers);
-    const houseSizePts = determineHouseSizePts(houseSize);
-    const total = houseHoldPTS + houseSizePts;
-    cfpData.push({    
-        houseHoldMembers: houseHoldMembers,
-        houseSize: houseSize,
-        houseHoldPTS: houseHoldPTS,
-        houseSizePts: houseSizePts,
-        total: total
-    });
-
-  }
-  
-  function displayOutput() {
-    const output = document.getElementById("output");
-    for (obj of cfpData) {
-      const newH2 = document.createElement("h2");
-      const newH3 = document.createElement("h3")
-      const newP = document.createElement("newP")
-      newH2.textContent = `My carbon footprint total is ${obj.total}`
-      newH3.textContent = `Based on Number in household and size of home`
-      newP.textContent = `This number is based on the number of the household of ${obj.houseHoldMembers} (score: ${obj.houseHoldPTS})`
-      newP.textContent += `and a ${obj.houseSize} size of home (score ${obj.houseSizePts})`
-      output.appendChild(newH2)
-      output.appendChild(newH3)
-      output.appendChild(newP)
-
-    }
-  }
-  
-  /*start(1, "apt");
-  start(2, "apt");
-  start(3, "apt");
-  start(4, "apt");
-  start(5, "apt");
-  start(6, "apt");
-  start(7, "apt");
-  start(1, "small");
-  start(2, "small");
-  start(3, "small");
-  start(4, "small");
-  start(5, "small");
-  start(6, "small");
-  start(7, "small");
-  start(1, "medium");
-  start(2, "medium");
-  start(3, "medium");
-  start(4, "medium");
-  start(5, "medium");
-  start(6, "medium");
-  start(7, "medium");
-  start(1, "large");
-  start(2, "large");
-  start(3, "large");
-  start(4, "large");
-  start(5, "large");
-  start(6, "large");
-  start(7, "large"); 
-  
- displayOutput()
- */
+*/
+TBL.appendChild(renderTblHeading());
